c612bf0f262c0b674c7b7c128e0520512e69625b
oskal314 <artem-osipovartem3002@mail.ru>
2022-12-18 12:44:48 +0300

    update sidebar and passwordChange

--- src/pages/home/passwordСhange/PasswordСhange.tsx
+++ src/pages/home/passwordСhange/PasswordСhange.tsx
@@ -1,22 +1,60 @@
-import React from 'react'
 import type { FC } from "react";
-import { Box } from '@mui/material';
+import {
+  Grid,
+  Paper,
+  Avatar,
+  TextField,
+  Button,
+  Typography,
+} from "@mui/material";
+import HttpsIcon from "@mui/icons-material/Https";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import { Link } from "react-router-dom";
 
-export const PasswordСhange: FC = ({  }) => {
+export const PasswordСhange: FC = ({}) => {
+  const paperStyle = {
+    padding: 20,
+    height: "70vh",
+    width: 480,
+    margin: "10px auto",
+  };
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
+  const btnstyle = { margin: "40px 0", padding: 20 };
+  const linkTo = {color:'white', textDecoration:'none'}
   return (
-    <div>
-      
-      <Box
-        sx={{
-          width: 300,
-          height: 300,
-          backgroundColor: "primary.dark",
-          "&:hover": {
-            backgroundColor: "primary.main",
-            opacity: [0.9, 0.8, 0.7],
-          },
-        }}
-      />
-    </div>
+    <Grid sx={{ pt: 10 }}>
+      <Paper elevation={10} style={paperStyle}>
+        <Grid2 sx={{ pt: 10 }}>
+          <Avatar style={avatarStyle}>
+            <HttpsIcon />
+          </Avatar>
+          <Typography variant="h4" sx={{ pt: 2 }}>
+            Поменяйте пароль
+          </Typography>
+        </Grid2>
+        <Typography sx={{ pt: 2 }}>
+          <TextField type="password" label="Пароль" fullWidth required />
+        </Typography>
+        <Typography sx={{ pt: 2 }}>
+          <TextField
+            label="Повторите пароль"
+            type="password"
+            fullWidth
+            required
+          />
+        </Typography>
+        <Button
+          type="submit"
+          color="primary"
+          variant="contained"
+          style={btnstyle}
+          fullWidth
+        >
+          <Link style={linkTo} to="/">
+            Войти
+          </Link>
+        </Button>
+      </Paper>
+    </Grid>
   );
 };
